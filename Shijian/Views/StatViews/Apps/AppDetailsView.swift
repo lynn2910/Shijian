547181e80977dacd2d9545f7438df663a7ff632f
lynn2910 <cedric.colin35@gmail.com>
2025-03-20 16:35:38 +0100

    Add basic charts to see times

--- Shijian/Views/StatViews/Apps/AppDetailsView.swift
+++ Shijian/Views/StatViews/Apps/AppDetailsView.swift
@@ -1,4 +1,5 @@
 import SwiftUI
+import Charts
 
 struct AppDetailsView: View {
     var app: AppInfo
@@ -9,6 +10,7 @@ struct AppDetailsView: View {
     
     var body: some View {
         VStack(alignment: .center) {
+            // header
             HStack {
                 Spacer()
                 
@@ -30,6 +32,50 @@ struct AppDetailsView: View {
                 }
             }
             
+            // Graphique d'utilisation
+            Chart(times) { time in
+                switch (period) {
+                    case .today:
+                        BarMark(
+                            x: .value("Heure", time.date),
+                            y: .value("Temps", time.time / 60 / 24)
+                        )
+                    case .last7Days:
+                        BarMark(
+                            x: .value("Jour", time.date),
+                            y: .value("Temps", time.time / 60 / 24 / 7)
+                        )
+                    default:
+                        BarMark(
+                            x: .value("Mois", time.date),
+                            y: .value("Temps", time.time / 60 / 24)
+                        )
+                }
+            }
+            .chartXAxis {
+                switch(period) {
+                    case .today:
+                        AxisMarks(values: .stride(by: .hour)) { value in
+                            AxisValueLabel(format: .dateTime.hour())
+                        }
+                    case .last7Days:
+                        AxisMarks(values: .stride(by: .day, count: 7)) { value in
+                            AxisValueLabel(format: .dateTime.day())
+                        }
+                    case .lastMonth:
+                        AxisMarks(values: .stride(by: .day, count: 30)) { value in
+                            AxisValueLabel(format: .dateTime.day())
+                        }
+                    case .last12Months:
+                    AxisMarks(values: .stride(by: .month, count: 12)) { value in
+                            AxisValueLabel(format: .dateTime.month())
+                        }
+                }
+            }
+                .padding()
+                .frame(height: 200)
+            
+            
             Spacer()
         }
     }
@@ -41,7 +87,7 @@ struct AppDetailsView: View {
     return AppDetailsView(
         app: AppInfo.testData.first!,
         times: AppInfo.testData.first!.times,
-        period: .last12Months,
+        period: .last7Days,
         shouldOpenSheet: $shouldOpenSheet
     )
 }
